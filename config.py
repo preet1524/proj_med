3513fdb7219501cdcde3412fd1ef3c3b73631e71
preet1524 <kamblepriti1995@gmail.com>
2022-10-30 14:20:54 +0530

    added config

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+
+CSV_FILE_PATH = "enter_path"
+
+PORT_NUMBER = 2002
+
+MODEL_FILE_PATH = os.path.join("model", "Bengaluru_House_data.pkl")
+
+JSON_FILE_PATH = os.path.join("model", "project_data.json")
